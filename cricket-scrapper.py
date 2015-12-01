b52842919b6d2301257a8877e96cec600139aeab
abhishekscript <abhishekscript@gmail.com>
2015-12-01 20:08:18 +0530

    Create cricket-scrapper.py

---
+++ cricket-scrapper.py
@@ -0,0 +1,15 @@
+from bs4 import *
+import urllib2,time,os
+while True:
+	print "Reading Score....."
+	url = "http://vcricket.com"
+	link = urllib2.urlopen(url).read()
+	os.system('cls')
+	soup = BeautifulSoup(link,"html.parser")
+	s=soup.find('span',{ "class" : 'brclr'} )
+	print "Current Score Is",s.text
+	print s.text
+	time.sleep(10)
+	os.system('cls')
+	
+	
