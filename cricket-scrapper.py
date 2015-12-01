8fa2fa6ef64693f0e1162d812618d806ae309ca8
abhishekscript <abhishekscript@gmail.com>
2015-12-01 20:09:10 +0530

    Update cricket-scrapper.py

--- cricket-scrapper.py
+++ cricket-scrapper.py
@@ -4,12 +4,12 @@ while True:
 	print "Reading Score....."
 	url = "http://vcricket.com"
 	link = urllib2.urlopen(url).read()
-	os.system('cls')
+	os.system('cls')       # Linux use 'clear'
 	soup = BeautifulSoup(link,"html.parser")
 	s=soup.find('span',{ "class" : 'brclr'} )
 	print "Current Score Is",s.text
 	print s.text
 	time.sleep(10)
-	os.system('cls')
+	os.system('cls')    # Linux  use 'clear'  
 	
 	
